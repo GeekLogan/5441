a7d633ddd43751e2c1b35866f985c86869bf8606
Logan Walker <walker.1698@buckeyemail.osu.edu>
2018-02-15 14:18:16 -0500

    Much faster

--- pa1-p1.c
+++ pa1-p1.c
@@ -111,11 +111,17 @@ void pa1p1opt(int n, double m[][n], double x[n], double y[n], double z[n])
 // Initially identical to reference; make your changes to optimize this code
 {
 	int i,j;
+
 	for( i=0; i<n; i++ )
 		for( j=0; j<n; j++ ) {
-			y[j] += m[i][j]*x[i];
-			z[j] += m[j][i]*x[i];
+			y[j] += m[i][j] * x[i];
 	}
+
+	for( j=0; i<n; i++ )
+		for( i=0; j<n; j++ ) {
+			z[j] += m[j][i] * x[i];
+	}
+
 }
 
 
