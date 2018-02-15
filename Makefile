7af5f97054860ac4ce7d5dec03f8d628ca65f92c
Logan Walker <walker.1698@buckeyemail.osu.edu>
2018-02-15 15:01:12 -0500

    Added loop unrolling

--- Makefile
+++ Makefile
@@ -4,6 +4,12 @@ default:
 	icc -O3 -DENABLE_PAPI -lpapi -qopt-report=2 -qopt-report-phase=vec pa1-p1.c
 	mv a.out pa1-p1.icc
 
+buildfull:
+	gcc -O3 -DENABLE_PAPI -lpapi -ftree-vectorizer-verbose=5 pa1-p1.c >& pa1-p1.gccvecrpt
+	mv a.out pa1-p1.gcc
+	icc -O3 -DENABLE_PAPI -lpapi -qopt-report=5 -qopt-report-phase=vec pa1-p1.c
+	mv a.out pa1-p1.icc
+
 test:
 	./pa1-p1.gcc
 	./pa1-p1.icc
--- pa1-p1.c
+++ pa1-p1.c
@@ -111,16 +111,25 @@ void pa1p1(int n, double m[][n], double x[n], double y[n], double z[n]) {
 	}
 }
 
-void pa1p1opt(int n, double m[][n], double x[n], double y[n], double z[n])
+void pa1p1opt( int n, double m[][n], double x[n], double y[n], double z[n] )
 // Initially identical to reference; make your changes to optimize this code
 {
-	int i,j;
+	int i, j;
 
-	for(i=0;i<n;i++)
-	for(j=0;j<n;j++) {
-		y[j] = y[j] + m[i][j]*x[i];
-		z[j] = z[j] + m[j][i]*x[i];
+	for( i=0; i<n; i++ ) {
+		for( j=0; j<n; j+=2 ) {
+			y[j] += m[i][j] * x[i];
+			y[j+1] += m[i][j+1] * x[i];
+		}
 	}
+
+	for( j=0; j<n; j++ ) {
+		for( i=0; i<n; i+=2 ) {
+			z[j] += m[j][i] * x[i];
+			z[j] += m[j][i + 1] * x[i + 1];
+		}
+	}
+
 }
 
 
